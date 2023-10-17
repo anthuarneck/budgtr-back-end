220d65e44f0bc38559a3624cf634a05875f1268c
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-10-16 22:09:27 -0400

    adds faker functionality.

--- models/transactions.js
+++ models/transactions.js
@@ -1,4 +1,38 @@
+const { faker } = require("@faker-js/faker");
+
 module.exports = [
+  {
+    id: 0,
+    item_name: "",
+    amount: 0,
+    date: `${faker.date.anytime()}`,
+    from: "",
+    category: "",
+  },
+  {
+    id: 0,
+    item_name: "",
+    amount: 0,
+    date: "",
+    from: "",
+    category: "",
+  },
+  {
+    id: 0,
+    item_name: "",
+    amount: 0,
+    date: "",
+    from: "",
+    category: "",
+  },
+  {
+    id: 0,
+    item_name: "",
+    amount: 0,
+    date: "",
+    from: "",
+    category: "",
+  },
   {
     id: 0,
     item_name: "",
